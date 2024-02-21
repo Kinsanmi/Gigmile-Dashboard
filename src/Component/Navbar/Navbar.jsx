4900591715bbd44da60baf1421303f4e71c9aa38
Kinsanmi <akinsanmimichael3@gamil.com>
2024-02-22 00:11:51 +0100

    Fixed search icon

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Navbar.scss";
-import { IoIosSearch } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { MdOutlineNoteAlt } from "react-icons/md";
@@ -20,7 +19,14 @@ export const Navbar = ({ sideBar, sideBarNav }) => {
 
         <div className="input-text">
           <div className="input">
-            <IoIosSearch className="icon" />
+            <svg className="icon"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"  />
+            </svg>
             <input
               type="text"
               placeholder="Search"
