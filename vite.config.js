832ec342ca14b5c8c78c79349997b3e1b592cc08
Kinsanmi <akinsanmimichael3@gamil.com>
2024-02-23 20:32:49 +0100

    dist corrected

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Gigmile-Dashboard/",
   plugins: [react()],
   server: {
     host: true,
